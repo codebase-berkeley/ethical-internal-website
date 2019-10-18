d329723669752f9c83aff63588dcf03594ffed04
micahelinor <mferas@berkeley.edu>
2019-10-17 21:34:58 -0700

    made post components for Announcements and a iterator

--- src/components/Announcements.js
+++ src/components/Announcements.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import Post from './Post'
+
+const postValues = [
+  {
+    title: 'New Inventory',
+    date: '10/17/19',
+    content: 'Hello, I am buying things right now'
+  },
+  {
+    title: 'New Application',
+    date: '10/20/19',
+    content: 'Finding New Members. Application is now open.'
+  },
+];
 
 class Announcements extends React.Component {
   render() {
-    return 
-    <div className = "Header" >
-    </div>;
+    return (
+      <div className = 'Announcements-section'>
+        {postValues.map((item) => (<Post {...item}/>))}
+      </div>
+    );
   }
 }
 
