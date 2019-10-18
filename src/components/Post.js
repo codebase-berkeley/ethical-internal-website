21adbc9a97acfe116664536d135afed9531b4616
micahelinor <mferas@berkeley.edu>
2019-10-17 21:30:37 -0700

    made post.js and post.css and changed announcement.js

---
+++ src/components/Post.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Post extends React.Component {
+  render() {
+    return (
+      <div className = "Post-box" >
+      <h1 className = "Post-title">
+        (this.props.title)
+      </h1>
+      <h1 className = "Post-date">
+        (this.props.date)
+      </h1>
+      <p className = "Post-content">
+        (this.props.content)
+      </p>
+    </div>
+    );
+  }
+}
+export default Post;
