3e7ecbaf32ffc0dc40276762bee9514e2ce2cc98
Katelyn Schmidt <17schmidtkatelyn@berkeley.edu>
2019-10-18 18:27:53 -0700

    Has a table, not cute

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,11 +1,87 @@
 import React from 'react';
+import './Orders.css';
+import DataTable from './DataTable';
+
+import {
+  useTable,
+  useGroupBy,
+  useFilters,
+  useSortBy,
+  useExpanded,
+  usePagination
+} from 'react-table'
+
+// function Table({ columns, data }) {
+//   // Use the state and functions returned from useTable to build your UI
+//   const {
+//     getTableProps,
+//     getTableBodyProps,
+//     headerGroups,
+//     rows,
+//     prepareRow,
+//   } = useTable({
+//     columns,
+//   })}
+
+//   const columns = React.useMemo(
+//     () => [
+//       {
+//         Header: 'Orders',
+//         columns: [
+//           {
+//             Header: 'Name',
+//             accessor: 'name',
+//           },
+//           {
+//             Header: 'Item',
+//             accessor: 'item',
+//           },
+//           {
+//             Header: 'Quantity',
+//             accessor: 'quantity',
+//           },
+//           {
+//             Header: 'Size',
+//             accessor: 'size',
+//           },
+//         ],
+//       }
+//     ],
+//     []
+//   )
 
 class Orders extends React.Component {
   render() {
+    const headings = [
+      'Name',
+      'Item',
+      'Quantity',
+      'Size',
+    ];
+
+    const rows = [
+      [
+        'Trevor Aquino',
+        'The Shirt',
+        2,
+        'M',
+      ],
+      [
+        'Parth Shah',
+        'The Other Shirt',
+        1,
+        'L',
+      ],
+    ];
     return (
-      <p>Orders!</p>
+      <div className="Orders">
+        <DataTable headings={headings} rows={rows}/>
+      </div>
+      
     );
   }
 }
+
+
 
 export default Orders;
