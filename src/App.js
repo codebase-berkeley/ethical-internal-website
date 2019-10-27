f19c2fe5ee69f69443104445733570c3bfdb995f
micahelinor <mferas@berkeley.edu>
2019-10-27 08:47:27 -0700

    add the routing feature on App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Announcements from "./components/Announcements";
 import Inventory from "./components/Inventory";
 import Orders from "./components/Orders";
 import NavigationBar from "./components/NavigationBar";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
-      <Announcements />
-      <Inventory />
-      <Orders />
+      <Switch>
+        <Route component={Announcements} path="/announcements" />
+        <Route component={Inventory} path="/inventory" />
+        <Route component={Orders} path="/orders" />
+      </Switch>
     </div>
   );
 }
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,16 +1,25 @@
 import React from "react";
 import "./NavigationBar.css";
+import { Link } from "react-router-dom";
 
 class NavigationBar extends React.Component {
   render() {
     return (
       <div className="Nav-bar">
         <ul>
-          <li>Announcements</li>
-          <li>Inventory</li>
-          <li>Orders</li>
+          <li>
+            <Link to="announcements">Announcements</Link>
+          </li>
+          <li>
+            <Link to="inventory">Inventory</Link>
+          </li>
+          <li>
+            <Link to="orders">Orders</Link>
+          </li>
           <div className="left">
-            <li>Ethical</li>
+            <li>
+              <Link to="/">Ethical</Link>
+            </li>
           </div>
         </ul>
       </div>
