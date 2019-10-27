940a3e522d4a6651c2a1ddadb98b44702075e3f7
Katelyn Schmidt <17schmidtkatelyn@berkeley.edu>
2019-10-26 19:40:37 -0700

    cleaned up unused code

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -2,54 +2,6 @@ import React from "react";
 import "./Orders.css";
 import DataTable from "./DataTable";
 
-// import {
-//   useTable,
-//   useGroupBy,
-//   useFilters,
-//   useSortBy,
-//   useExpanded,
-//   usePagination
-// } from "react-table";
-
-// function Table({ columns, data }) {
-//   // Use the state and functions returned from useTable to build your UI
-//   const {
-//     getTableProps,
-//     getTableBodyProps,
-//     headerGroups,
-//     rows,
-//     prepareRow,
-//   } = useTable({
-//     columns,
-//   })}
-
-//   const columns = React.useMemo(
-//     () => [
-//       {
-//         Header: 'Orders',
-//         columns: [
-//           {
-//             Header: 'Name',
-//             accessor: 'name',
-//           },
-//           {
-//             Header: 'Item',
-//             accessor: 'item',
-//           },
-//           {
-//             Header: 'Quantity',
-//             accessor: 'quantity',
-//           },
-//           {
-//             Header: 'Size',
-//             accessor: 'size',
-//           },
-//         ],
-//       }
-//     ],
-//     []
-//   )
-
 class Orders extends React.Component {
   constructor() {
     super();
@@ -60,14 +12,8 @@ class Orders extends React.Component {
   async componentDidMount() {
     const response = await fetch("http://localhost:3000/orders");
     const json = await response.json();
-    //console.log(response.body.read());
     this.setState({ output: json.map(elem => elem) });
   }
-  // updateStats(){
-  //   this.setState({
-  //     output: this.state.output
-  //   })
-  // }
 
   render() {
     const headings = [
@@ -81,15 +27,7 @@ class Orders extends React.Component {
     ];
     const api_rows = this.state.output;
     console.log(api_rows);
-    // for (let i = 0; i < 4; i++) {
 
-    //   const rows =[]
-    // }
-    //for
-    // const rows = [
-    //   ["Trevor Aquino", "The Shirt", 2, "M"],
-    //   ["Parth Shah", "The Other Shirt", 1, "L"]
-    // ];
     return (
       <div className="Orders">
         <DataTable headings={headings} rows={api_rows} />
