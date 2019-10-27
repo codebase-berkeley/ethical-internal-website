a11673bb3400ea6e06c5562cd7e917357a0944e8
micahelinor <mferas@berkeley.edu>
2019-10-27 08:18:02 -0700

    added classNames to ul and li

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,59 +1,21 @@
 import React from "react";
 import "./NavigationBar.css";
-import { Route, Switch, Link } from "react-router-dom";
 
 class NavigationBar extends React.Component {
   render() {
     return (
       <div className="Nav-bar">
         <ul>
-          <li>
-            <Link to="/announcements">Announcements</Link>
-          </li>
-          <li>
-            <Link to="/inventory">Inventory</Link>
-          </li>
-          <li>
-            <Link to="/orders">Orders</Link>
-          </li>
-          <li className="left">
-            <Link to="/">Ethical</Link>
-          </li>
+          <li>Announcements</li>
+          <li>Inventory</li>
+          <li>Orders</li>
+          <div className="left">
+            <li>Ethical</li>
+          </div>
         </ul>
-
-        <Switch>
-          <Route path="/announcements">
-            <Announcements />
-          </Route>
-          <Route path="/inventory">
-            <Inventory />
-          </Route>
-          <Route path="/orders">
-            <Orders />
-          </Route>
-          <Route path="/">
-            <Ethical />
-          </Route>
-        </Switch>
       </div>
     );
   }
-}
-
-function Announcements() {
-  return <h2>Announcements</h2>;
-}
-
-function Orders() {
-  return <h2>Orders</h2>;
-}
-
-function Inventory() {
-  return <h2>Inventory</h2>;
-}
-
-function Ethical() {
-  return <h2>Home</h2>;
 }
 
 export default NavigationBar;
