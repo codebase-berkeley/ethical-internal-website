a8ec8a512d95764818805d49903853275cefc522
蔡祐臻 <kellytsai@calvisitor-10-105-187-197.calvisitor.1918.berkeley.edu>
2019-11-01 00:48:15 -0700

    response can be logged from backend, but frontend gives out empty list

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,6 +1,5 @@
 import React from "react";
 import DataTable from "./DataTable";
-const axios = require("axios");
 
 class Inventory extends React.Component {
   constructor() {
@@ -11,15 +10,13 @@ class Inventory extends React.Component {
   }
 
   async componentDidMount() {
-    var that = this;
-    axios
-      .get("http://localhost:3001/inventory")
-      .then(function(response) {
-        that.setState({ api_rows: response.data });
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
+    const response = await fetch("http://localhost:3001/inventory");
+    console.log(await fetch("http://localhost:3001/inventory"));
+    // console.log(response);
+    const json = await response.json();
+    // console.log(json);
+    this.setState = { api_rows: json };
+    // console.log(this.state.api_rows);
   }
 
   render() {
@@ -39,3 +36,45 @@ class Inventory extends React.Component {
 }
 
 export default Inventory;
+
+// import React from "react";
+// import DataTable from "./DataTable";
+// const axios = require("axios");
+
+// class Inventory extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       api_rows: []
+//     };
+//   }
+
+//   async componentDidMount() {
+//     var that = this;
+//     axios
+//       .get("http://localhost:3001/inventory")
+//       .then(function(response) {
+//         that.setState({ api_rows: response.data });
+//       })
+//       .catch(function(error) {
+//         console.log(error);
+//       });
+//   }
+
+//   render() {
+//     if (this.state.api_rows !== []) {
+//       const headings = ["Item", "Count", "Price", "Sold"];
+
+//       return (
+//         <div>
+//           <div className="Inventory">
+//             <h1>Inventory</h1>
+//             <DataTable headings={headings} rows={this.state.api_rows} />
+//           </div>
+//         </div>
+//       );
+//     }
+//   }
+// }
+
+// export default Inventory;
