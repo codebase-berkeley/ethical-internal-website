a963cc9c1ef427287c8f844f14afc71ddf1033d0
James Li <superpokemon888@gmail.com>
2019-11-02 23:21:16 -0700

    rearranged into array of objects

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -2,25 +2,6 @@ import React from "react";
 import "./Orders.css";
 import DataTable from "./DataTable";
 
-function ArrayToObject() {
-  var arr = [
-    "pickUpDate",
-    "last",
-    "first",
-    "orderNum",
-    "size_style",
-    "itemOrdered",
-    "itemQuantity"
-  ];
-  //var objList = [];
-  var obj = {};
-
-  for (var i = 0; i < arr.length; i++) {
-    obj[arr[i]] = this.state.orderRows.map(data => data[i]);
-  }
-  return obj;
-}
-
 class Orders extends React.Component {
   constructor() {
     super();
@@ -34,75 +15,8 @@ class Orders extends React.Component {
     this.setState({ orderRows: json.map(elem => elem) });
   }
 
-  /*export function makeData(len = 5553) {
-    return range(len).map(d => {
-      return {
-        ...newPerson(),
-        children: range(10).map(newPerson)
-      };
-    });
-  }*/
-
-  /*
-  function funcName(params) {
-   return params + 2;
- }
-  funcName(2);
-  // 4
-
-  var funcName = (params) => params + 2
-  funcName(2);
-  // 4*/
-
   render() {
-    var arr = [
-      "pickUpDate",
-      "last",
-      "first",
-      "orderNum",
-      "size_style",
-      "itemOrdered",
-      "itemQuantity"
-    ];
-    var orderLst = [];
-    const nestedObjects = () => {
-      //var objList = [];
-      var obj = {};
-      /*for (var i = 0; i < this.state.orderRows.length; i++) {
-        obj[arr[i]] = this.state.orderRows.map(data =>
-          data.map(value => value)
-        );
-      }*/
-      var i = 0;
-      while (i < this.state.orderRows.length) {
-        orderLst[i] = this.state.orderRows.map(data => {
-          for (var i = 0; i < arr.length; i++) {
-            obj[arr[i]] = data[i];
-          }
-          i++;
-        });
-      }
-      return obj;
-    };
-
-    const newPerson = () => {
-      const statusChance = Math.random();
-      return {
-        firstName: "james",
-        lastName: "li",
-        age1: Math.floor(Math.random() * 30),
-        visits: Math.floor(Math.random() * 100),
-        progress: Math.floor(Math.random() * 100),
-        status:
-          statusChance > 0.66
-            ? "relationship"
-            : statusChance > 0.33
-            ? "complicated"
-            : "single"
-      };
-    };
-
-    console.log(nestedObjects());
+    //console.log(this.state.orderRows);
     const headings = [
       "Pick Up Date",
       "Last",
@@ -112,6 +26,19 @@ class Orders extends React.Component {
       "Item Ordered",
       "Item Quantity"
     ];
+
+    var arrayOfObjects = this.state.orderRows.map(function(item) {
+      return {
+        "Pick Up Date": item[0],
+        Last: item[1],
+        First: item[2],
+        "Order#": item[3],
+        "Size/Style": item[4],
+        "Item Ordered": item[5],
+        "Item Quantity": item[6]
+      };
+    });
+    console.log(arrayOfObjects);
 
     return (
       <div className="Orders">
