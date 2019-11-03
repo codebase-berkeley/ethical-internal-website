b9273fa3e41c08cc6c50496a70e4311840275237
trev4ev <trevoraquino@gmail.com>
2019-11-03 13:42:59 -0800

    update backend

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3001;
-const cors = require("cors");
-const fetch = require("node-fetch");
-import { userId, basicAuth } from "./config";
+const cors = require('cors');
+const fetch = require('node-fetch');
+const {userId, basicAuth, userName, userPassword} = require('./config');
 
 app.use(cors());
 
-app.get("/inventory", function(req, res) {
+app.get('/inventory', function(req, res) {
   getId().then(result => res.send(result));
 });
 
@@ -15,14 +15,14 @@ async function getId() {
   let idList = [];
 
   const response = await fetch(
-    "https://api.bigcartel.com/v1/accounts/" + userId + "/products",
+    'https://api.bigcartel.com/v1/accounts/' + userId + '/products',
     {
       headers: {
-        Authorization: "Basic " + basicAuth,
-        "Content-type": "application/vnd.api+json",
-        Accept: "application/vnd.api+json"
-      }
-    }
+        Authorization: 'Basic ' + basicAuth,
+        'Content-type': 'application/vnd.api+json',
+        Accept: 'application/vnd.api+json',
+      },
+    },
   );
   const json = await response.json();
   async function getInventory() {
@@ -30,14 +30,14 @@ async function getId() {
     for (let i = 0; i < json.data.length; i++) {
       idList.push(json.data[i].relationships.options.data[0].id);
     }
-    var includedList = json.included.filter(e => e.type == "product_options");
+    var includedList = json.included.filter(e => e.type == 'product_options');
     for (let i = 0; i < idList.length; i++) {
       var obj = includedList.filter(e => e.id == idList[i]);
       var x = [
         json.data[i].attributes.name,
         obj[0].attributes.quantity,
         obj[0].attributes.price,
-        obj[0].attributes.sold
+        obj[0].attributes.sold,
       ];
       list.push(x);
     }
@@ -46,4 +46,4 @@ async function getId() {
   return await getInventory();
 }
 
-app.listen(port, () => console.log("listening! server started"));
+app.listen(port, () => console.log('listening! server started'));
