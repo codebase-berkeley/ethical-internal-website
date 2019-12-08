cfa39d5395cd4ff0132d581d8c947c3888b29140
micahelinor <mferas@berkeley.edu>
2019-12-08 14:33:31 -0800

    changed login to announcements

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
             <Route component={NavigationBar} path="/" />
             <Route component={Orders} path="/" />
           </Route>
-          <Redirect from="/" to="/login" />
+          <Redirect from="/" to="/announcements" />
         </Switch>
       </div>
     </div>
