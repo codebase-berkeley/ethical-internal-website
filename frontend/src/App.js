3ee1b7758d1dd311005eb8428bc60935291ba730
Amitav Baruah <amitav@berkeley.edu>
2020-01-14 13:14:13 -0800

    organization & default page at / is orders for use

---
+++ frontend/src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./App.css";
+import Announcements from "./components/Announcements";
+import Inventory from "./components/Inventory";
+import Orders from "./components/Orders";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Login from "./components/Login";
+import NavigationBar from "./components/NavigationBar";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/announcements">
+            <Route component={NavigationBar} path="/" />
+            <Route component={Announcements} path="/" />
+          </Route>
+          <Route path="/inventory">
+            <Route component={NavigationBar} path="/" />
+            <Route component={Inventory} path="/" />
+          </Route>
+          <Route path="/orders">
+            <Route component={NavigationBar} path="/" />
+            <Route component={Orders} path="/" />
+          </Route>
+          <Route path="/">
+            <Route component={NavigationBar} path="/" />
+            <Route component={Orders} path="/" />
+          </Route>
+        </Switch>
+      </div>
+    </div>
+  );
+}
+
+export default App;
